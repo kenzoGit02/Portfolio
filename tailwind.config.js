b8ceac431b6e535eb51df31671b481e1573fbb1f
kenzo <kenzorivas11@gmail.com>
2024-09-05 20:17:51 +0800

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily:{
-        test:['New Amsterdam', 'sans serif']
+        amsterdam:['New Amsterdam', 'sans serif']
       }
     },
   },
